e3463adbe6e48e5a2507874ad87cd888908c50e6
Jay Andrae Salvador <jay.andrae@playsport.com>
2020-03-29 12:50:59 +1100

    initial commit

---
+++ Stan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stan
+//
+//  Created by Jay Salvador on 25/3/20.
+//  Copyright © 2020 Jay Salvador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
